315e30db7df1d7e7890b21f54280283f6e443646
Sanyad11 <veherasasha98@gmail.com>
2020-01-08 21:15:45 +0200

    Added Models and Models View

---
+++ Workers/Models View/Vacation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workers.ModelsView
+{ 
+    public class VacationTwo
+    {
+      
+        public string startDay { get; set; }
+        public string EndDay { get; set; }
+    }
+}
